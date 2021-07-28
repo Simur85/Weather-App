70fb4ac942d3de12a1e7cd0968d2ae53393a3a87
Simur85.Daniel <Simur85.dsimur@gmail.com>
2021-07-28 17:47:09 -0500

    gral rodriguez por el freezer

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,6 +4,7 @@ import style from './Card.module.css'
 
 
 export default function Card({name, temp, flag, img, onClose,id}) {
+    if(name === 'General Rodríguez') name = 'El Freezer'
     if(name === 'Cúcuta') name = 'Calielandia'
     if(name === 'Mazatlán') name = 'Peponópolis'
     if(name === 'New York') name = 'Diego York'
